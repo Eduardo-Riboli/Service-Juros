6cc3502b460a549f954ed2f2eeb05c39959efab6
Eduardo Riboli <EduardoRiboli71@gmail.com>
2022-02-28 17:09:54 -0300

    Services

--- Curso_OO/Services/OnlinePaymentService.cs
+++ Curso_OO/Services/OnlinePaymentService.cs
@@ -1,7 +1,7 @@
 ﻿namespace Curso_OO.Services
 {
     interface IOnlinePaymentService
-    {
+    { 
         double PaymentFee(double amount);
         double Interest(double amount, int months);
     }
--- Curso_OO/Services/PayPalService.cs
+++ Curso_OO/Services/PayPalService.cs
@@ -10,5 +10,5 @@
         {
             return (amount * 0.01) * months;
         }
-    }
+    } 
 }
